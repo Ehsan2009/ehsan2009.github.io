8661dd8aefec2fa1b7e18747ac5d851d2e003400
Ehsan Javdan <ehsanjavdan77@gmail.com>
2024-10-12 12:34:21 +0400

    finished authentication screen

--- lib/config/app_router.dart
+++ lib/config/app_router.dart
@@ -1,10 +1,15 @@
 import 'package:chat_app/screens/auth_screen.dart';
+import 'package:chat_app/screens/splash_screen.dart';
 import 'package:go_router/go_router.dart';
 
 final router = GoRouter(
   routes: [
     GoRoute(
       path: '/',
+      builder: (context, state) => const SplashScreen(),
+    ),
+    GoRoute(
+      path: '/auth_screen',
       builder: (context, state) => const AuthScreen(),
     ),
   ],
--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -1,4 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class AuthScreen extends StatefulWidget {
@@ -12,14 +15,62 @@ class _AuthScreenState extends State<AuthScreen> {
   final formKey = GlobalKey<FormState>();
   var enteredEmail = '';
   var enteredPassword = '';
-  var enteredConfirmPassword = '';
+  var isLogin = false;
+  var isAuthenticating = false;
+  var passwordController = TextEditingController();
 
-  void submit() {}
+  @override
+  void dispose() {
+    passwordController.dispose();
+    super.dispose();
+  }
+
+  void submit() async {
+    bool validate = formKey.currentState!.validate();
+
+    if (!validate) {
+      return;
+    }
+
+    formKey.currentState!.save();
+
+    try {
+      setState(() {
+        isAuthenticating = true;
+      });
+      if (isLogin) {
+        await FirebaseAuth.instance.signInWithEmailAndPassword(
+          email: enteredEmail.trim(),
+          password: enteredPassword.trim(),
+        );
+      } else {
+        await FirebaseAuth.instance.createUserWithEmailAndPassword(
+          email: enteredEmail.trim(),
+          password: enteredPassword.trim(),
+        );
+
+        await FirebaseFirestore.instance
+            .collection('users')
+            .doc(FirebaseAuth.instance.currentUser!.uid)
+            .set({'email': enteredEmail});
+      }
+
+      context.go('/');
+    } on FirebaseAuthException catch (error) {
+      ScaffoldMessenger.of(context).clearSnackBars();
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text(error.message ?? 'Authentication failed.')),
+      );
+      setState(() {
+        isAuthenticating = false;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     final height = MediaQuery.of(context).size.height;
-    
+
     return Scaffold(
       backgroundColor: Colors.grey[300],
       body: SingleChildScrollView(
@@ -40,7 +91,9 @@ class _AuthScreenState extends State<AuthScreen> {
                   const SizedBox(height: 24),
 
                   Text(
-                    'Let\'t create an account for you',
+                    isLogin
+                        ? 'Welcome back you\'ve been missed!'
+                        : 'Let\'t create an account for you',
                     style: GoogleFonts.roboto(
                       fontSize: 18,
                       color: Colors.grey[700],
@@ -71,13 +124,24 @@ class _AuthScreenState extends State<AuthScreen> {
                         ),
                       ),
                     ),
+                    validator: (value) {
+                      if (value!.isEmpty || !value.contains('@')) {
+                        return 'Please enter a valid email address';
+                      }
+                      return null;
+                    },
+                    onSaved: (value) {
+                      enteredEmail = value!;
+                    },
                   ),
 
                   const SizedBox(height: 16),
 
                   // password TextFormFields
                   TextFormField(
+                    controller: passwordController,
                     cursorColor: Colors.black,
+                    obscureText: true,
                     decoration: InputDecoration(
                       hintText: 'Password',
                       hintStyle: TextStyle(color: Colors.grey[500]),
@@ -97,33 +161,65 @@ class _AuthScreenState extends State<AuthScreen> {
                         ),
                       ),
                     ),
+                    validator: (value) {
+                      if (value!.isEmpty || value.trim().length < 6) {
+                        return 'Please enter a valid password with atleast 6 characters';
+                      }
+                      return null;
+                    },
+                    onSaved: (value) {
+                      enteredPassword = value!;
+                    },
                   ),
 
-                  const SizedBox(height: 16),
+                  if (isLogin) const SizedBox(height: 8),
 
-                  // password confirm TextFormFields
-                  TextFormField(
-                    cursorColor: Colors.black,
-                    decoration: InputDecoration(
-                      hintText: 'Confirm password',
-                      hintStyle: TextStyle(color: Colors.grey[500]),
-                      fillColor: Colors.white54,
-                      filled: true,
-                      floatingLabelBehavior: FloatingLabelBehavior.never,
-                      focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(16),
-                        borderSide: const BorderSide(
-                          color: Colors.white,
-                        ),
-                      ),
-                      enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(16),
-                        borderSide: const BorderSide(
-                          color: Colors.white,
+                  if (isLogin)
+                    Align(
+                      alignment: Alignment.centerRight,
+                      child: Text(
+                        'Forgot Password?',
+                        style: GoogleFonts.roboto(
+                          fontSize: 14,
+                          color: Colors.grey[700],
                         ),
                       ),
                     ),
-                  ),
+
+                  if (!isLogin) const SizedBox(height: 16),
+
+                  // password confirm TextFormFields
+                  if (!isLogin)
+                    TextFormField(
+                      cursorColor: Colors.black,
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        hintText: 'Confirm password',
+                        hintStyle: TextStyle(color: Colors.grey[500]),
+                        fillColor: Colors.white54,
+                        filled: true,
+                        floatingLabelBehavior: FloatingLabelBehavior.never,
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(16),
+                          borderSide: const BorderSide(
+                            color: Colors.white,
+                          ),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(16),
+                          borderSide: const BorderSide(
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                      validator: (value) {
+                        if (value!.isEmpty ||
+                            value != passwordController.text) {
+                          return 'Password is not correct';
+                        }
+                        return null;
+                      },
+                    ),
 
                   const SizedBox(height: 24),
 
@@ -138,11 +234,13 @@ class _AuthScreenState extends State<AuthScreen> {
                         color: Colors.white70,
                         borderRadius: BorderRadius.circular(16),
                       ),
-                      child: Text(
-                        'Sign Up',
-                        style: GoogleFonts.roboto(
-                            fontSize: 20, fontWeight: FontWeight.bold),
-                      ),
+                      child: isAuthenticating
+                          ? const CircularProgressIndicator()
+                          : Text(
+                              isLogin ? 'Login' : 'Sign Up',
+                              style: GoogleFonts.roboto(
+                                  fontSize: 20, fontWeight: FontWeight.bold),
+                            ),
                     ),
                   ),
 
@@ -153,15 +251,22 @@ class _AuthScreenState extends State<AuthScreen> {
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
                       Text(
-                        'Already a member?',
+                        isLogin ? 'Not a member' : 'Already a member?',
                         style: GoogleFonts.roboto(color: Colors.grey[700]),
                       ),
                       const SizedBox(width: 5),
-                      Text(
-                        'Login now',
-                        style: GoogleFonts.roboto(
-                          color: Colors.black,
-                          fontWeight: FontWeight.bold,
+                      GestureDetector(
+                        onTap: () {
+                          setState(() {
+                            isLogin = !isLogin;
+                          });
+                        },
+                        child: Text(
+                          isLogin ? 'Register now' : 'Login now',
+                          style: GoogleFonts.roboto(
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                          ),
                         ),
                       ),
                     ],
