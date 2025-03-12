6d423207ac9f128f4522811873f855607665bd18
Ehsan Javdan <ehsanjavdan77@gmail.com>
2025-03-12 13:33:29 +0400

    Updates

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f73bfc4522dd0bc87bbcdb4bb3088082755c5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4177428330"
+    serviceWorkerVersion: "1857900071"
   }
 });
